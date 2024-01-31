ccd5e2a2788cc52933132aa4b85de7bc7c9b2cb1
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2024-01-31 22:54:26 +0600

    added UserRepository

---
+++ src/main/java/kg/alatoo/ecommerce/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package kg.alatoo.ecommerce.repositories;
+
+import kg.alatoo.ecommerce.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
