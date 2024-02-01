bdc2fe4ed25040b36b9f93a4d30b28d29a78dc9c
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2024-02-02 00:11:20 +0600

    added Category entity

--- src/main/java/kg/alatoo/ecommerce/controllers/TestController.java
+++ src/main/java/kg/alatoo/ecommerce/controllers/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello Chushpan!";
     }
 
     @GetMapping("/bye")
