e2fc34ec683d2ddd2013ef05adfcee9ec6d6c4e3
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2024-02-08 13:01:05 +0600

    test commit

--- src/main/java/kg/alatoo/ecommerce/controllers/TestController.java
+++ src/main/java/kg/alatoo/ecommerce/controllers/TestController.java
@@ -21,4 +21,9 @@ public class TestController {
     public String salam(){
         return "Salam World!";
     }
+
+    @GetMapping("/salam2")
+    public String salam2(){
+        return "Salam World!";
+    }
 }
