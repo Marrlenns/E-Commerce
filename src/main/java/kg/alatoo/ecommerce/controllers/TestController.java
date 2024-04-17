d8c21f571d8978c019ea0c3adaf55904f645eddc
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2024-04-17 16:32:16 +0600

    for raiym

--- src/main/java/kg/alatoo/ecommerce/controllers/TestController.java
+++ src/main/java/kg/alatoo/ecommerce/controllers/TestController.java
@@ -1,20 +1,29 @@
 package kg.alatoo.ecommerce.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final MessageSource messageSource;
     //Dordoi eto che
     @GetMapping("/hello")
     public String hello(){
-        return "Hello Chushpan!";
+        return messageSource.getMessage("common.hello", null, LocaleContextHolder.getLocale());
     }
 
     @GetMapping("/bye")
-    public String bye(){
-        return "Good Bye World!";
+    public String bye(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("common.hello", null, LocaleContextHolder.getLocale());
     }
 
     @GetMapping("/salam")
@@ -26,4 +35,5 @@ public class TestController {
     public String salam2(){
         return "Salam World!";
     }
+
 }
